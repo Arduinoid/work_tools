17b85389214781cb592b0ee0ea596d270ab2df3a
unknown <Jon@JON-T3500.main.TECHVITALITY.com>
2016-01-28 09:47:56 -0500

    Changed the way steps are defined. They are now dynamic

--- Multi-watermark.py
+++ Multi-watermark.py
@@ -14,15 +14,15 @@ sys.stdout.write("\b" * (toolbar_width+1))
 # sets opacity of the water mark
 opacity = 1
 amount = 50
-# Change this variable to adjust the amount that the watermark steps
-# accross the image
-step = 50
+# this is the predicted width of the water mork text
+text_width = 400
 # selecting the file that is a jpeg amoung other files
 img_file = files[img_location]
 # Splitting the file into file name and extension name
 name, extension = img_file.split(".")
 # opens image file
 base = Image.open(img_file).convert('RGBA')
+step = (base.size[0] - text_width) / amount
 # make a blank image for the text, initialized to transparent text color
 txt = Image.new('RGBA', base.size, (255,255,255,0))
 
@@ -31,16 +31,18 @@ fnt = ImageFont.truetype('arial.ttf', 60)
 # new drawing object
 d = ImageDraw.Draw(txt)
 
-# Loop that creates each unique photo
-position = 0
-for i in range(amount):
-    position += step
-    d.text((position,10), "SaveMyServer", font=fnt, fill=(0,0,0,opacity))
-    out = Image.alpha_composite(base, txt)
-    out.save(name + "-watermarked-" + str(i + 1)+ "." + extension)
-    txt = Image.new('RGBA', base.size, (255,255,255,0))
-    d = ImageDraw.Draw(txt)
-    sys.stdout.write("#")
-    sys.stdout.flush()
+if __name__ == '__main__':
+    
+    # Loop that creates each unique photo
+    position = 0
+    for i in range(amount):
+        position += step
+        d.text((position,10), "SaveMyServer", font=fnt, fill=(0,0,0,opacity))
+        out = Image.alpha_composite(base, txt)
+        out.save(name + "-watermarked-" + str(i + 1)+ "." + extension)
+        txt = Image.new('RGBA', base.size, (255,255,255,0))
+        d = ImageDraw.Draw(txt)
+        sys.stdout.write("#")
+        sys.stdout.flush()
 
-sys.stdout.write("\n")
+    sys.stdout.write("\n")
